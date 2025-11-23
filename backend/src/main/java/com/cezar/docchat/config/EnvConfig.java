84395210619bbf583f2387e7311a038145800894
Cézar Novaes <cezarddt@gmail.com>
2025-11-23 11:24:30 -0300

    feat: configuracoes e build

--- backend/src/main/java/com/cezar/docchat/config/EnvConfig.java
+++ backend/src/main/java/com/cezar/docchat/config/EnvConfig.java
@@ -1,7 +1,6 @@
 package com.cezar.docchat.config;
 
 import org.springframework.context.annotation.Configuration;
-
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 
@@ -11,6 +10,12 @@ public class EnvConfig {
     @PostConstruct
     public void loadEnv() {
         Dotenv dotenv = Dotenv.configure().load();
+        
+        // Carrega JWT_SECRET
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION", "86400000"));
+        
+        // Carrega OPENAI_API_KEY
         System.setProperty("OPENAI_API_KEY", dotenv.get("OPENAI_API_KEY"));
     }
 }
